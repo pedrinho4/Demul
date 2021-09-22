ed38e9f68d75ac82d4ea0a978bcd0d0ce3a30d8a
Luis Game Player <64819941+pedrinho4@users.noreply.github.com>
2021-09-21 21:45:53 -0300

    Add files via upload

---
+++ Arcade_NAOMI.c
@@ -0,0 +1,18 @@
+ "include<stdio.h>
+
+   "int  main(){
+         printf("\n"):
+         
+   }    char nome[16] = {0}:
+         printf(" Diga Seu Nome:  "):
+         Scanf("%s".nome):
+         printf("\n"):
+         
+         printf("Arcade. %s! .NAOMI):
+         
+         
+         
+         
+         printf("\n\n"):
+         return 0:
+         }
